1e1766d4bb602ecd7ccccedd14f36de4fd57fe39
morinpic <morinpic0518@gmail.com>
2018-09-06 17:24:09 +0900

    add jquery via cdn

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
       footer
     </footer>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/common.js"></script>
     <?php wp_footer(); ?>
   </body>
